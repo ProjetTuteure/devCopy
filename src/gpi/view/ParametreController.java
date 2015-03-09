db015cc5aa38f39fcd1607a521f03916c6aeccb0
limmilkki <totor-du-40@hotmail.fr>
2015-03-09 16:40:30 +0100

    bouton

--- src/gpi/view/ParametreController.java
+++ src/gpi/view/ParametreController.java
@@ -44,7 +44,7 @@ public class ParametreController implements Initializable {
 	}
 	
 	@FXML
-	public void onClickButton(){
+	public void onClickButtonRapport(){
 		Rapports rapport=new Rapports();
 		rapport.GenerateFinGarantie();
 	}
