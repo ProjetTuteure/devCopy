753d4f9addf2c13344ba0e9613cc724713e6a804
JulienPlas <Julien.plas19@live.fr>
2015-03-11 16:08:40 +0100

    Composition

--- src/gpi/view/SupprimerUtilisation.java
+++ src/gpi/view/SupprimerUtilisation.java
@@ -5,11 +5,13 @@ import gpi.metier.PageMateriel;
 import gpi.metier.PageMaterielDAO;
 import gpi.metier.Utilisateur;
 import gpi.metier.UtilisateurDAO;
+import gpi.metier.Utilise;
 import gpi.metier.UtiliseDAO;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import utils.DateConverter;
 import utils.Popup;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -25,63 +27,87 @@ public class SupprimerUtilisation {
 	private boolean okClicked = false;
 
 	@FXML
-	private ComboBox<String> ComboboxNomUtilisateur;
+	private ComboBox<String> comboboxNomUtilisateur;
 	@FXML
-	private ComboBox<String> ComboboxMateriel;
+	private ComboBox<String> comboboxMateriel;
+	@FXML
+	private ComboBox<String> comboboxDateUtilisation;
 	
 	private ObservableList<String> listNomUtilisateur;
 	private List<Integer> listIdUtilisateur;
 	private ObservableList<String> listNomMateriel;
-	private List<String> listIdMateriel;
-	private ObservableList<String> listprenom;
+	private List<Integer> listIdMateriel;
+	private ObservableList<String> listDateUtilisation;
+	private List<String> listDateUtilisationDB;
 
 	private int idUtilisateur;
 
-	private String idMateriel;
+	private int idMateriel;
+	
+	private String dateUtilisation;
 	
 	/**
-	 * Initialise les donn�es
+	 * Initialise les données
 	 */
 	@FXML
 	private void initialize() {
-		UtilisateurDAO utilisateurDAO = new UtilisateurDAO();
-		PageMaterielDAO pageMaterielDAO = new PageMaterielDAO();
+		UtiliseDAO utiliseDAO = new UtiliseDAO();
 		listNomUtilisateur = FXCollections.observableArrayList();
 		listIdUtilisateur = new ArrayList<Integer>();
 		listNomMateriel = FXCollections.observableArrayList();
-		listIdMateriel = new ArrayList<String>();
+		listIdMateriel = new ArrayList<Integer>();
 		
 		try{
-			for(Utilisateur utilisateur : utilisateurDAO.recupererAllUtilisateur()){
-				listNomUtilisateur.add(utilisateur.getNomUtilisateur().getValue()+" "+utilisateur.getPrenomUtilisateur().getValue());
-				listIdUtilisateur.add(utilisateur.getIdUtilisateur().intValue());
-			}
-			for(PageMateriel pageMateriel : pageMaterielDAO.getAllMateriel()){
-				listNomMateriel.add(pageMateriel.getNomMateriel());
-				listIdMateriel.add(pageMateriel.getIdMateriel());	
+			for(Utilise utilise : utiliseDAO.recupererMaterielUtiliseAll()){
+				listNomUtilisateur.add(utilise.getUtilisateurUtilise().getNomUtilisateur().getValue()+" "+utilise.getUtilisateurUtilise().getPrenomUtilisateur().getValue());
+				listIdUtilisateur.add(utilise.getUtilisateurUtilise().getIdUtilisateur().intValue());
 			}
 		}catch(ConnexionBDException e){
 			Popup.getInstance().afficherPopup(e.getMessage());
 		}
-		ComboboxNomUtilisateur.setItems(listNomUtilisateur);
-		ComboboxMateriel.setItems(listNomMateriel);
+		comboboxNomUtilisateur.setItems(listNomUtilisateur);
+		comboboxMateriel.setItems(listNomMateriel);
 	}
 
 	@FXML
 	private void handleChange() {
-		this.idUtilisateur = (listIdUtilisateur.get(ComboboxNomUtilisateur.getSelectionModel().getSelectedIndex()));
+		this.idUtilisateur = (listIdUtilisateur.get(comboboxNomUtilisateur.getSelectionModel().getSelectedIndex()));
 		PageMaterielDAO pageMaterielDAO = new PageMaterielDAO();
 		this.listNomMateriel = FXCollections.observableArrayList();
-		this.listIdMateriel = new ArrayList<String>();
+		this.listIdMateriel = new ArrayList<Integer>();
+		this.listDateUtilisation = FXCollections.observableArrayList();
+		listDateUtilisationDB = new ArrayList<String>();
 		try {
 			for(PageMateriel pageMateriel : pageMaterielDAO.getMaterielByIdUtilisateur(idUtilisateur)){
 				listNomMateriel.add(pageMateriel.getNomMateriel());
-				listIdMateriel.add(pageMateriel.getIdMateriel());	
+				listIdMateriel.add(Integer.parseInt(pageMateriel.getIdMateriel()));	
 			}
 		} catch (ConnexionBDException e) {
 			Popup.getInstance().afficherPopup(e.getMessage());
 		}
-		ComboboxMateriel.setItems(listNomMateriel);
+		comboboxMateriel.setItems(listNomMateriel);
+	}
+	
+	@FXML
+	private void handleChange1() {
+		if(comboboxMateriel.getSelectionModel().getSelectedIndex()!=-1){
+			this.idUtilisateur = listIdUtilisateur.get(comboboxNomUtilisateur.getSelectionModel().getSelectedIndex());
+			this.idMateriel = listIdMateriel.get(comboboxMateriel.getSelectionModel().getSelectedIndex());
+			System.out.println(idUtilisateur);
+			System.out.println(idMateriel);
+			UtiliseDAO utiliseDAO = new UtiliseDAO();
+			this.listDateUtilisation = FXCollections.observableArrayList();
+			listDateUtilisationDB = new ArrayList<String>();
+			try {
+				for(String date : utiliseDAO.recupererDateUtilisation(idMateriel, idUtilisateur)){
+					listDateUtilisation.add(DateConverter.getFrenchDate(date));
+					listDateUtilisationDB.add(date);
+				}
+			} catch (ConnexionBDException e) {
+				Popup.getInstance().afficherPopup(e.getMessage());
+			}
+			comboboxDateUtilisation.setItems(listDateUtilisation);
+		}
 	}
 	
 	/**
@@ -110,12 +136,15 @@ public class SupprimerUtilisation {
 	 */
 	@FXML
 	private void handleOk() {
-		UtiliseDAO utiliseDAO = new UtiliseDAO();
-		this.idUtilisateur = (listIdUtilisateur.get(ComboboxNomUtilisateur.getSelectionModel().getSelectedIndex()));
-		this.idMateriel = (listIdMateriel.get(ComboboxMateriel.getSelectionModel().getSelectedIndex()));
+		UtiliseDAO utiliseDAO=new UtiliseDAO();
+		this.idUtilisateur = (listIdUtilisateur.get(comboboxNomUtilisateur.getSelectionModel().getSelectedIndex()));
+		this.idMateriel = (listIdMateriel.get(comboboxMateriel.getSelectionModel().getSelectedIndex()));
+		this.dateUtilisation = (listDateUtilisationDB.get(comboboxDateUtilisation.getSelectionModel().getSelectedIndex()));
+		System.out.println(dateUtilisation);
+		
 		try {
-			utiliseDAO.supprimerUtlise(this.idUtilisateur, Integer.parseInt(this.idMateriel));
-			Popup.getInstance().afficherPopup("Utilisateur "+ComboboxNomUtilisateur.getValue()+" supprimé du matériel "+ComboboxMateriel.getValue());
+			utiliseDAO.supprimerUtilise(this.idUtilisateur, this.idMateriel,this.dateUtilisation);
+			Popup.getInstance().afficherPopup("Utilisateur "+comboboxNomUtilisateur.getValue()+" supprimé du matériel "+comboboxMateriel.getValue());
 		} catch (NumberFormatException | ConnexionBDException e) {
 			/*Todo*/
 		}
