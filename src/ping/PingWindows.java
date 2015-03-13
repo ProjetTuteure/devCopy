7e164c22f13010aaa0133522c0d7aa5e2feab535
cedricCaffy <cedric.caffy@hotmail.fr>
2015-03-13 11:53:36 +0100

    PING QUI MARCHAIT PAS REFACTOR :+1:

--- src/ping/PingWindows.java
+++ src/ping/PingWindows.java
@@ -1,5 +1,9 @@
 package ping;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import gpi.metier.Materiel;
 
 public class PingWindows implements Ping,Runnable {
@@ -16,35 +20,35 @@ public class PingWindows implements Ping,Runnable {
 	public void run()
 	{
 		synchronized(this){
-		try {
-			this.wait();
-			ping(materiel);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+			try {
+				this.wait();
+				ping(materiel);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 	
+	/**
+	 * Réalise un ping sur un matériel
+	 * @param materiel le matériel dont on veut tester le ping
+	 */
 	@Override
 	public synchronized void ping(Materiel materiel) {
-		try {  
-			ProcessBuilder pb=new ProcessBuilder("cmd.exe","/c","ping","-n","1",materiel.getNomMateriel().getValue());
-			Process p=pb.start();
-	        int exitValue = p.waitFor();
-	        if(exitValue==0)
-	        {
-	        	this.resultatPing=true;
-	        	this.notify();
-	        }
-	        else
-	        {
-	        	this.resultatPing=false;
-	        	this.notify();
-	        }
-		} catch (Exception e) {
-			e.printStackTrace();
-		} 
+		boolean status=false;
+		int timeOut=3000;
+		try {
+			status = InetAddress.getByName (materiel.getNomMateriel().getValue()).isReachable(timeOut);
+			} catch (UnknownHostException e) {}
+			catch (IOException e) {} 
+		if(status==true){
+			this.resultatPing=true;
+			this.notify();
+		}else{
+			this.resultatPing=false;
+			this.notify();
+		}
 	}
 	
 	public synchronized boolean getResultatPing()
